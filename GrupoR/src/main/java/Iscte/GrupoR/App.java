433f85c689a595c17b218a4a1986084fb154d439
RicardoMinascurta <145024185+RicardoMinascurta@users.noreply.github.com>
2023-10-23 13:30:46 +0100

    Update App.java

--- GrupoR/src/main/java/Iscte/GrupoR/App.java
+++ GrupoR/src/main/java/Iscte/GrupoR/App.java
@@ -3,7 +3,7 @@ package Iscte.GrupoR;
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("Hello WorldASASDAXSER");
+		System.out.println("Hello WorldASAfgtfgSDAXSER");
 	}
 	
 }
