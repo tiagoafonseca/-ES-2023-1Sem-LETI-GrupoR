6cea90d0963851ba0bb00f4278e6254b2d597b3e
tiagoafonseca <tiago.a.afonseca@gmail.com>
2023-11-05 19:21:15 +0000

    update openfilecsv.java
    semi-update (erro ao abrir no pc do tiago)

--- GrupoR/src/main/java/Iscte/GrupoR/OpenFileCSV.java
+++ GrupoR/src/main/java/Iscte/GrupoR/OpenFileCSV.java
@@ -1,63 +1,95 @@
 package Iscte.GrupoR;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-	import javax.swing.*;
-	import java.awt.*;
-	import java.awt.event.*;
-	import java.io.*;
-	import java.util.ArrayList;
-	import java.util.List;
+public class OpenFileCSV {
 
-	public class OpenFileCSV {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Ler Arquivo CSV Manualmente");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1080, 720);
 
-	    public static void main(String[] args) {
-	        JFrame frame = new JFrame("Ler Arquivo CSV Manualmente");
-	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	        frame.setSize(1080, 720);
+        JPanel panel = new JPanel();
+        frame.add(panel);
 
-	        JPanel panel = new JPanel();
-	        frame.add(panel);
+        JButton openButton = new JButton("Escolher Arquivo CSV");
+        panel.add(openButton);
 
-	        JButton openButton = new JButton("Escolher Arquivo CSV");
-	        panel.add(openButton);
+        openButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser fileChooser = new JFileChooser();
+                int returnValue = fileChooser.showOpenDialog(null);
 
-	        openButton.addActionListener(new ActionListener() {
-	            @Override
-	            public void actionPerformed(ActionEvent e) {
-	                JFileChooser fileChooser = new JFileChooser();
-	                int returnValue = fileChooser.showOpenDialog(null);
+                if (returnValue == JFileChooser.APPROVE_OPTION) {
+                    File selectedFile = fileChooser.getSelectedFile();
+                    try {
+                        BufferedReader reader = new BufferedReader(new FileReader(selectedFile));
+                        // Defina a ordem esperada das colunas
+                        String[] expectedColumns = {"Curso", "Unidade Curricular", "Turno", "Turma",
+                                "Inscritos no turno", "Dia da semana", "Hora de início da aula", "Hora de fim da aula",
+                                "Data da aula", "Características da sala de pedida para a aula",
+                                "Sala atribuída à aula"};
 
-	                if (returnValue == JFileChooser.APPROVE_OPTION) {
-	                    File selectedFile = fileChooser.getSelectedFile();
-	                    try {
-	                        BufferedReader reader = new BufferedReader(new FileReader(selectedFile));
-	                        String line;
-	                        List<String[]> records = new ArrayList<>();
+                        String firstLine = reader.readLine();
+                        if (firstLine != null) {
+                            String[] actualColumns = firstLine.split("[,;]");
 
-	                        while ((line = reader.readLine()) != null) {
-	                            String[] fields = line.split(",");
-	                            records.add(fields);
-	                        }
+                            Map<String, Integer> columnIndices = new HashMap<>();
+                            for (int i = 0; i < actualColumns.length; i++) {
+                                columnIndices.put(actualColumns[i].trim(), i);
+                            }
 
-	                        for (String[] record : records) {
-	                            for (String field : record) {
-	                                System.out.print(field);
-	                            }
-	                            System.out.println();
-	                        }
+                            // Organize as colunas com base na ordem esperada
+                            String[] orderedColumns = new String[expectedColumns.length];
+                            for (int i = 0; i < expectedColumns.length; i++) {
+                                orderedColumns[i] = actualColumns[columnIndices.get(expectedColumns[i])];
+                            }
 
-	                        reader.close();
-	                    } catch (IOException ex) {
-	                        ex.printStackTrace();
-	                    }
-	                 // Fecha a janela após carregar o arquivo
-	                    frame.dispose();
-	                }
-	            }
-	        });
+                            System.out.println(Arrays.toString(orderedColumns));
 
-	        frame.setVisible(true);
-	    }
-	}
+                            List<String[]> records = new ArrayList<>();
 
+                            String line;
+                            int linesToRead = 5;
+                            int currentLine = 0;
 
+                            while ((line = reader.readLine()) != null && currentLine < linesToRead) {
+                                String[] fields = line.split("[,;]");
+                                // Organize os campos de acordo com a ordem esperada
+                                String[] orderedFields = new String[expectedColumns.length];
+                                for (int i = 0; i < expectedColumns.length; i++) {
+                                    orderedFields[i] = fields[columnIndices.get(expectedColumns[i])];
+                                }
+                                records.add(orderedFields);
+                                currentLine++;
+                            }
+
+                            for (String[] record : records) {
+                                System.out.println(Arrays.toString(record));
+                            }
+
+                            reader.close();
+                        }
+
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+
+                // Fecha a janela após carregar o arquivo
+                frame.dispose();
+            }
+        });
+
+        frame.setVisible(true);
+    }
+}
