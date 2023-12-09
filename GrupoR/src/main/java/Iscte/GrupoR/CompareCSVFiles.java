d3b20a154f6e507cab2c299431628e5685326ac3
rraoo3iscte <rraoo3@iscte-iul.pt>
2023-12-09 22:51:58 +0000

    Update 2.5
    Parte 1 dos pontos 4.1 e 5.1

--- GrupoR/src/main/java/Iscte/GrupoR/CompareCSVFiles.java
+++ GrupoR/src/main/java/Iscte/GrupoR/CompareCSVFiles.java
@@ -1,7 +1,6 @@
 package Iscte.GrupoR;
 
 import javax.swing.*;
-import java.awt.*;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -15,60 +14,70 @@ import java.util.Map;
 public class CompareCSVFiles {
 
 	public static void main(String[] args) {
-		// Carrega o primeiro arquivo CSV
 		File firstFile = chooseCSVFile();
 		List<String[]> firstFileData = readCSV(firstFile);
 
-		// Carrega o segundo arquivo CSV
 		File secondFile = chooseCSVFile();
 		List<String[]> secondFileData = readCSV(secondFile);
 
-		// Cria um mapa para armazenar a Capacidade Normal com base nas salas
 		Map<String, String> salaCapacidadeMap = new HashMap<>();
 
 		for (String[] row : secondFileData.subList(1, secondFileData.size())) {
 			String sala = (row.length > 1) ? row[1] : "-";
 			String capacidadeNormal = (row.length > 2) ? row[2] : "-";
+
 			salaCapacidadeMap.put(sala, capacidadeNormal);
 		}
 
-		// Cria uma tabela de saída
 		List<String[]> outputTable = new ArrayList<>();
-		outputTable.add(new String[] { "Turma", "Dia", "Hora Inicio", "Sala Atribuida", "Numero Inscritos",
-				"Caracteristicas da Sala", "Colunas com 'X'", "Caracteristicas Sala Pedida" });
+		outputTable.add(new String[] { "Turma", "Dia", "Hora Inicio", "Data Aula", "Sala Atribuida", "Numero Inscritos",
+				"Número de Lugares Normal", "Colunas com 'X'", "Caracteristicas da Sala Pedida", "Ideal=0/Falso=1",
+				"ComparacaoInscritosLugares", "SomaUltimasColunas", "ResultadoFinal" });
 
-		// Preenche a tabela de saída
 		for (String[] firstRow : firstFileData.subList(1, firstFileData.size())) {
 			String salaAtribuida = (firstRow.length > 9) ? firstRow[9] : "-";
-
 			String turma = (firstRow.length > 3) ? firstRow[3] : "-";
 			String dia = (firstRow.length > 5) ? firstRow[5] : "-";
 			String horaInicio = (firstRow.length > 6) ? firstRow[6] : "-";
 			String numeroInscritos = (firstRow.length > 4) ? firstRow[4] : "-";
+			String numLugaresNormal = salaCapacidadeMap.getOrDefault(salaAtribuida, "--");
+			String dataaula = (firstRow.length > 7) ? firstRow[7] : "-";
+			String CaracteristicaSalaPedida = (firstRow.length > 10) ? firstRow[10] : "";
+			String colunasComX = getColunasComX(secondFileData, salaAtribuida, CaracteristicaSalaPedida);
 
-			// Obtém a Capacidade Normal do mapa (se existir)
-			String capacidadeNormal = salaCapacidadeMap.getOrDefault(salaAtribuida, "--");
+			// Comparar cada string individual com a característica da sala pedida
+			String[] caracteristicasArray = colunasComX.split("\\|");
+			int resultadoComparacao = 1; // Inicializar como 1 (se não houver correspondência)
 
-			// Obtém as características da sala a partir da coluna 5
-			StringBuilder caracteristicasSala = new StringBuilder();
-			for (int i = 5; i < firstRow.length; i++) {
-				if ("x".equalsIgnoreCase(firstRow[i])) {
-					caracteristicasSala.append(" ").append(firstFileData.get(0)[i]);
+			for (String caracteristica : caracteristicasArray) {
+				if (caracteristica.trim().compareToIgnoreCase(CaracteristicaSalaPedida) == 0) {
+					resultadoComparacao = 0;
+					break; // Se encontrar uma correspondência, atribuir 0 e interromper o loop
 				}
 			}
 
-			// Adiciona à tabela de saída
-			outputTable.add(new String[] { turma, dia, horaInicio, salaAtribuida, numeroInscritos, capacidadeNormal,
-					getColunasComX(secondFileData, salaAtribuida), (firstRow.length > 10) ? firstRow[10] : "-" });
+			// Comparar "Numero Inscritos" e "Número de Lugares Normal"
+			int comparacaoInscritosLugares = compareInscritosLugares(numeroInscritos, numLugaresNormal);
+
+			// Calcular a soma das últimas duas colunas
+			int somaUltimasColunas = resultadoComparacao + comparacaoInscritosLugares;
+
+			// Determinar se é "Ideal" ou "Não Ideal" com base na soma
+			String resultadoFinal = (somaUltimasColunas == 0) ? "Ideal" : "Não Ideal";
+
+			outputTable.add(new String[] { turma, dia, horaInicio, dataaula, salaAtribuida, numeroInscritos,
+					numLugaresNormal, colunasComX, CaracteristicaSalaPedida, String.valueOf(resultadoComparacao),
+					String.valueOf(comparacaoInscritosLugares), String.valueOf(somaUltimasColunas), resultadoFinal });
 		}
 
-		// Exibe a tabela de saída
+		// Imprimir a tabela
 		for (String[] row : outputTable) {
 			System.out.println(Arrays.toString(row));
 		}
 	}
 
-	public static String getColunasComX(List<String[]> secondFileData, String salaAtribuida) {
+	public static String getColunasComX(List<String[]> secondFileData, String salaAtribuida,
+			String caracteristicaSalaPedida) {
 		StringBuilder colunasComX = new StringBuilder();
 
 		// Verifica se a lista não está vazia e se possui elementos suficientes
@@ -83,7 +92,7 @@ public class CompareCSVFiles {
 					for (int colIndex = 5; colIndex < secondRow.length; colIndex++) {
 						// Verifica se o índice é válido
 						if (colIndex < secondFileData.get(0).length && "X".equalsIgnoreCase(secondRow[colIndex])) {
-							colunasComX.append(secondFileData.get(0)[colIndex]).append(" ");
+							colunasComX.append(secondFileData.get(0)[colIndex]).append("|");
 						}
 					}
 					break; // Interrompe após encontrar a linha correspondente
@@ -94,48 +103,4 @@ public class CompareCSVFiles {
 		return colunasComX.toString().trim();
 	}
 
-	// Métodos de leitura de arquivo semelhantes aos do primeiro arquivo
-	private static List<String[]> readCSV(File file) {
-		List<String[]> data = new ArrayList<>();
-
-		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-			String line;
-
-			// Lê a linha de cabeçalho
-			line = br.readLine(); // Lê o cabeçalho
-			String[] header = line.split(";"); // Obtém os nomes das colunas
-			data.add(header);
-
-			// Lê as linhas do arquivo
-			while ((line = br.readLine()) != null) {
-				String[] values = line.split(";"); // Assumindo ponto e vírgula como delimitador
-				data.add(values);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		return data;
-	}
-
-	private static File chooseCSVFile() {
-		JFileChooser fileChooser = new JFileChooser();
-		int returnValue = fileChooser.showOpenDialog(null);
-
-		if (returnValue == JFileChooser.APPROVE_OPTION) {
-			return fileChooser.getSelectedFile();
-		}
-
-		return null;
-	}
-
-	public static int tryParseInt(String value, int defaultValue) {
-		try {
-			return Integer.parseInt(value);
-	    } catch (NumberFormatException e) {
-	        return defaultValue;
-	    }
-	}
 }
-
-
